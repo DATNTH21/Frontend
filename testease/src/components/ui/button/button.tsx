3421c29ed66c54e869ba78e70b561d6adbaf5af5
gld-ngocphung <ngocphung@golivedev.com>
2024-11-25 03:17:16 +0700

    HN: Resolve button conflict

--- testease/src/components/ui/button/button.tsx
+++ testease/src/components/ui/button/button.tsx
@@ -2,6 +2,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { forwardRef } from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cn } from '@/utils/cn';
+import { Spinner } from '../spinner/spinner';
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
@@ -31,18 +32,22 @@ const buttonVariants = cva(
   }
 );
 
-export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
-}
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+    isLoading?: boolean;
+    icon?: React.ReactNode;
+  };
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ children, variant, size, asChild = false, isLoading, icon, className, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
-    return <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />;
+    return (
+      <Comp className={cn(buttonVariants({ variant, size, className }))} {...props} ref={ref}>
+        {isLoading && <Spinner size='sm' className='text-current'></Spinner>}
+        {!isLoading && icon}
+        {children}
+      </Comp>
+    );
   }
 );
-Button.displayName = 'Button';
-
-export { Button, buttonVariants };

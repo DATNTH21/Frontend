5883c44e8c8f900bc1d60c53c3dfee3f69a52f10
21120420 <lehuynhcuong0701@gmail.com>
2024-11-25 08:36:03 +0700

    .

--- testease/src/app/(app)/all-project/page.tsx
+++ testease/src/app/(app)/all-project/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
@@ -20,26 +20,26 @@ const AllProjectPage = () => {
       id: 'PR-1',
       title: 'Demo project',
       link: '98 test cases',
-      description: 'This projecttttttttttttttttttttttttttttttttttt ahdioahdioabsnsafks',
+      description: 'This projecttttttttttttttttttttttttttttttttttt ahdioahdioabsnsafks'
     },
     {
       id: 'PR-2',
       title: 'Demo project',
       link: '98 test cases',
-      description: 'This projecttttttttttttttttttttttttttttttttttt ahdioahdioabsnsafks',
+      description: 'This projecttttttttttttttttttttttttttttttttttt ahdioahdioabsnsafks'
     },
     {
       id: 'PR-3',
       title: 'Demo project',
       link: '98 test cases',
-      description: 'This projecttttttttttttttttttttttttttttttttttt ahdioahdioabsnsafks',
+      description: 'This projecttttttttttttttttttttttttttttttttttt ahdioahdioabsnsafks'
     },
     {
       id: 'PR-4',
       title: 'Demo project',
       link: '98 test cases',
-      description: 'This projecttttttttttttttttttttttttttttttttttt ahdioahdioabsnsafks',
-    },
+      description: 'This projecttttttttttttttttttttttttttttttttttt ahdioahdioabsnsafks'
+    }
   ]);
 
   const handleSearch = () => {
@@ -70,15 +70,15 @@ const AllProjectPage = () => {
   return (
     <div className='w-full'>
       {/* Search Bar */}
-      <div className="mb-4 relative w-full">
-        <SearchIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-500" />
+      <div className='mb-4 relative w-full'>
+        <SearchIcon className='absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-500' />
         <input
-          type="text"
+          type='text'
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
           onKeyDown={handleKeyDown}
-          placeholder="Search projects..."
-          className="w-full pl-10 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder='Search projects...'
+          className='w-full pl-10 p-2 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500'
         />
       </div>
 
@@ -86,10 +86,10 @@ const AllProjectPage = () => {
       <Table>
         <TableHeader>
           <TableRow>
-            <TableHead className="w-[100px]">ID</TableHead>
+            <TableHead className='w-[100px]'>ID</TableHead>
             <TableHead>PROJECT TITLE</TableHead>
             <TableHead>QUICK LINK</TableHead>
-            <TableHead className="text-right"></TableHead>
+            <TableHead className='text-right'></TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -97,28 +97,27 @@ const AllProjectPage = () => {
             projects.map((project) => (
               <TableRow key={project.id}>
                 <TableCell
-                  className="font-medium cursor-pointer hover:bg-gray-100"
-                  onClick={() => navigateDashboard(project.id)}
-                >{project.id}</TableCell>
-                <TableCell 
-                  className="cursor-pointer hover:bg-gray-100"
+                  className='font-medium cursor-pointer hover:bg-gray-100'
                   onClick={() => navigateDashboard(project.id)}
                 >
+                  {project.id}
+                </TableCell>
+                <TableCell className='cursor-pointer hover:bg-gray-100' onClick={() => navigateDashboard(project.id)}>
                   <div>{project.title}</div>
                   <div>{project.description}</div>
                 </TableCell>
-                <TableCell 
-                  className="cursor-pointer hover:bg-gray-100"
+                <TableCell
+                  className='cursor-pointer hover:bg-gray-100'
                   onClick={() => navigateBlackboxTesting(project.id)}
                 >
                   {project.link}
                 </TableCell>
-                <TableCell className="text-right">ACTION</TableCell>
+                <TableCell className='text-right'>ACTION</TableCell>
               </TableRow>
             ))
           ) : (
             <TableRow>
-              <TableCell colSpan={4} className="text-center">
+              <TableCell colSpan={4} className='text-center'>
                 No projects found.
               </TableCell>
             </TableRow>
@@ -127,7 +126,7 @@ const AllProjectPage = () => {
         <TableFooter>
           <TableRow>
             <TableCell colSpan={3}>Total projects</TableCell>
-            <TableCell className="text-right">{projects.length}</TableCell>
+            <TableCell className='text-right'>{projects.length}</TableCell>
           </TableRow>
         </TableFooter>
       </Table>

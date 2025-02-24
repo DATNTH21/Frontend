60b9960a3e14f1c2106e60a5102e6397d4abc233
wendiago <phungngoctqk@gmail.com>
2025-02-24 17:42:06 +0700

    Fix sidebar footer

--- testease/src/components/layouts/Sidebar/sidebar-footer.tsx
+++ testease/src/components/layouts/Sidebar/sidebar-footer.tsx
@@ -102,7 +102,6 @@ export default function AppSidebarFooter() {
       ) : (
         <SidebarFooterSkeleton />
       )}
-      {isLoading && <LoadingOverlay spinner={<SolarSystem />} />}
     </>
   );
 }
